d41ac05c3feb5d1c6f8a1cda44eedb14623e8d0c
abdelhay-chtibi <142673856+abdelhay-chtibi@users.noreply.github.com>
2025-05-04 12:19:08 +0000

    seance2

---
+++ seance2/for.py
@@ -0,0 +1,12 @@
+print("---------------------Exemple 1 :---------------------------")
+#start, end, step
+for i in range(1, 10, 2):
+    print(i)
+
+print("---------------------Exemple 2 :---------------------------")
+for i in range(-4, 10, 3):
+    print(i)
+
+print("---------------------Exemple 3 :---------------------------")
+for i in range(12, 3, -1):
+    print(i)
